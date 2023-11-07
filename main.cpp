62be1c0cc7eb1bd27523d9399afc036f51134b64
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-11-07 15:18:14 -0300

    reformulação da main e mais opções de escolha

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
+#include "Item.cpp"
+#include "Inventario.cpp"
 #include <iostream>
-
+#include <fstream> // Para manipulação de arquivos
 
 int main() {
+    Inventario meuInventario;
     int n;
 
-    std::cout << "O que voce deseja fazer?" << std::endl;
-    std::cout << "1: Adicionar item" << std::endl;
-    std::cout << "2: Remover item" << std::endl;
-    std::cout << "3: Listar itens" << std::endl;
-    std::cout << "4: Editar item" << std::endl;
-    std::cout << "5: Ver valor total do estoque" << std::endl;
+    while (true) {
+        std::cout << "O que você deseja fazer?" << std::endl;
+        std::cout << "1: Cadastrar novo item" << std::endl;
+        std::cout << "2: Remover um item" << std::endl;
+        std::cout << "3: Editar item" << std::endl;
+        std::cout << "4: Adicionar itns" << std::endl;
+        std::cout << "5: Retiradr itens" << std::endl;
+        std::cout << "6: Listar itens" << std::endl;
+        std::cout << "7: Ver valor total do estoque" << std::endl;
+        std::cout << "8: Encerrar sessão" << std::endl;
 
-    // Leitura da escolha do usuário
-    std::cin >> n;
+        // Leitura da escolha do usuário
+        std::cin >> n;
 
-     if (n == 1) {
-        // Adicionar item
-    } else if (n == 2) {
-        // Remover item
-    } else if (n == 3) {
-        // Listar itens
-    } else if (n == 4) {
-        // Editar item
-    } else if (n == 5) {
-        // Ver valor total do estoque
-    } else {
-        std::cout << "Escolha invalida. Digite um numero de 1 a 5" << std::endl;
+        if (n == 1) {
+            // Cadastrar item
+            meuInventario.cadastrarItem();
+        } else if (n == 2) {
+            // Remover item
+            meuInventario.removerItem();
+        } else if (n == 3) {
+            // 
+
+        } else if (n == 4) {
+            // 
+            meuInventario.adicionarItens();
+        } else if (n == 5) {
+            // 
+            meuInventario.retirarItens();
+        } else if (n == 8) {
+            // Encerrar a sessão
+            break;  // Sai do loop para encerrar o programa
+        } else {
+            std::cout << "Escolha inválida. Digite um número válido" << std::endl;
+        }
     }
 
-    
     return 0;
 }    
